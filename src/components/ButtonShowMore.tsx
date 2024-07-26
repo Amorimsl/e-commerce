c27a511db6c812ae838b7a8dde32c6af3968efc7
Lucas Amorim <lucasamorim_10_@hotmail.com>
2024-07-26 02:42:06 -0300

    fix: buttonShowmore window(0,0)

--- src/components/ButtonShowMore.tsx
+++ src/components/ButtonShowMore.tsx
@@ -1,13 +1,21 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const ButtonShowMore = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/Shop');
+    window.scrollTo(0, 0);
+  };
+
   return (
-    <div>
-      <div className="text-center mt-6">
-        <button className="text-custom-text-yellow font-semibold border border-custom-text-yellow py-2 px-16">
-          <Link to="/Shop">Show More</Link>
-        </button>
-      </div>
+    <div className="text-center mt-6">
+      <button
+        className="text-custom-text-yellow font-semibold border border-custom-text-yellow py-2 px-16"
+        onClick={handleClick}
+      >
+        Show More
+      </button>
     </div>
   );
 };
